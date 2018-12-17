f250297f06a892aa3f71dba4d1527752463c25d6
nawhi <25394965+nawhi@users.noreply.github.com>
2018-12-17 14:14:51 +0000

    Add Spark and simple test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+import static spark.Spark.get;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello world");
+    }
+}
---
+++ src/test/java/SanityCheck.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SanityCheck {
+
+    @Test
+    public void test_the_truth() {
+        assertTrue(true);
+    }
+}
