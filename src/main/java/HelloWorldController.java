7d78e42602925c4588ecdadecb659214d80e14c1
nawhi <25394965+nawhi@users.noreply.github.com>
2018-12-17 14:49:30 +0000

    Set content type JSON in controller

--- src/main/java/HelloWorldController.java
+++ src/main/java/HelloWorldController.java
@@ -4,6 +4,7 @@ import spark.Response;
 public class HelloWorldController {
     public String helloWorld(Request req, Response res) {
         res.status(200);
+        res.type("application/json");
         return null;
     }
 }
--- src/test/java/HelloWorldControllerShould.java
+++ src/test/java/HelloWorldControllerShould.java
@@ -9,9 +9,9 @@ import static org.mockito.Mockito.verify;
 public class HelloWorldControllerShould {
     private Response res;
     private Request req;
+    private HelloWorldController controller;
 
     /*
-    1. status 200
     2. content type application/json
     3. convert content from internal format to JSON
      */
@@ -20,14 +20,18 @@ public class HelloWorldControllerShould {
     void setUp() {
         res = mock(Response.class);
         req = mock(Request.class);
+        controller = new HelloWorldController();
     }
 
     @Test
     public void set_status_200_for_hello_world_request() {
-        var controller = new HelloWorldController();
-
         controller.helloWorld(req, res);
-
         verify(res).status(200);
+    }
+
+    @Test
+    public void set_json_content_for_hello_world_request() {
+        controller.helloWorld(req, res);
+        verify(res).type("application/json");
     }
 }
